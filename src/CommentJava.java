37a72cdc9d703b2d39ed137fe201397fa8275157
doublekm153 <doublekm153@gmail.com>
2024-07-04 05:55:19 +0900

    기본 설정

---
+++ src/CommentJava.java
@@ -0,0 +1,15 @@
+public class CommentJava {
+  public static void main(String[] args) {
+    System.out.println("Hello java"); //hello java1 을 출력
+    System.out.println("Hello java"); //hello java2 을 출력
+    System.out.println("Hello java"); //hello java3 을 출력
+    /*
+    * System.out.println("Hello java"); //hello java4 을 출력
+    * System.out.println("Hello java"); //hello java5 을 출력
+    * */
+    
+    /* sout("Hello java"); //hello java6 을 출력
+    * sout("Hello java"); //hello java7 을 출력
+     */
+  }
+}
